037908ce323f22f07a2d1e8898698768e5377352
phillyai <sonagi.philly@gmail.com>
2018-09-22 02:53:27 +0900

    됨

---
+++ FakeYoutube/KeyHook.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FakeYoutube
+{
+    public static class KeyHook
+    {
+        public static event Action<RawKeyEventArgs> KeyDown;
+        public static event Action<RawKeyEventArgs> KeyUp;
+
+        static IntPtr hookId = IntPtr.Zero;
+        static InterceptKeys.LowLevelKeyboardProc proc;
+
+        static KeyHook()
+        {
+            proc = HookCallback;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                if (wParam == (IntPtr)InterceptKeys.WM_KEYDOWN)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    KeyDown?.Invoke(new RawKeyEventArgs(vkCode, false));
+                }
+                else if (wParam == (IntPtr)InterceptKeys.WM_KEYUP)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    KeyUp?.Invoke(new RawKeyEventArgs(vkCode, false));
+                }
+            }
+            return InterceptKeys.CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+        
+        public static void Start()
+        {
+            hookId = InterceptKeys.SetHook(proc);
+        }
+
+        public static void Stop()
+        {
+            InterceptKeys.UnhookWindowsHookEx(hookId);
+        }
+    }
+
+    internal static class InterceptKeys
+    {
+        public delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        public static int WH_KEYBOARD_LL = 13;
+        public static int WM_KEYDOWN = 0x0100;
+        public static int WM_KEYUP = 0x0101;
+
+        public static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+
+    public class RawKeyEventArgs : EventArgs
+    {
+        public int VKCode;
+        public Key Key;
+        public bool IsSysKey;
+
+        public RawKeyEventArgs(int VKCode, bool isSysKey)
+        {
+            this.VKCode = VKCode;
+            this.IsSysKey = isSysKey;
+            this.Key = KeyInterop.KeyFromVirtualKey(VKCode);
+        }
+    }
+}
--- FakeYoutube/MainWindow.xaml.cs
+++ FakeYoutube/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +11,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace FakeYoutube
 {
@@ -20,9 +26,102 @@ namespace FakeYoutube
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("gdi32.dll")]
+        static extern bool DeleteObject(IntPtr hObject);
+
+        Storyboard playAnim, pauseAnim;
+        BitmapImage pauseImg, playImg;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Closing += (o, e) => KeyHook.Stop();
+            KeyHook.KeyDown += KeyHook_KeyDown;
+            KeyHook.Start();
+
+            playAnim = FindResource("PlayAnim") as Storyboard;
+            pauseAnim = FindResource("PauseAnim") as Storyboard;
+            playAnim.Completed += PlayAnim_Completed;
+            pauseImg = new BitmapImage();
+            pauseImg.BeginInit();
+            pauseImg.UriSource = new Uri("Pause.png", UriKind.Relative);
+            pauseImg.EndInit();
+            playImg = new BitmapImage();
+            playImg.BeginInit();
+            playImg.UriSource = new Uri("Play.png", UriKind.Relative);
+            playImg.EndInit();
+            HideMe();
+        }
+
+        private void PlayAnim_Completed(object sender, EventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+        }
+
+        private void KeyHook_KeyDown(RawKeyEventArgs key)
+        {
+            if (key.Key == Key.F8)
+                Switch();
+        }
+
+        void Switch()
+        {
+            if (Visibility == Visibility.Hidden)
+                ShowMe();
+            else
+                HideMe();
+        }
+
+        void ShowMe()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            this.Topmost = true;
+            this.Activate();
+            this.grid.Background = CopyScreen();
+            image.Source = pauseImg;
+            BeginStoryboard(pauseAnim);
+            Visibility = Visibility.Visible;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Switch();
+        }
+
+        void HideMe()
+        {
+            this.grid.Background = Brushes.Transparent;
+            image.Source = playImg;
+            BeginStoryboard(playAnim);
+            // GC.Collect();
+        }
+
+        static ImageBrush CopyScreen()
+        {
+            using (var screenBmp = new Bitmap(
+                (int)SystemParameters.PrimaryScreenWidth,
+                (int)SystemParameters.PrimaryScreenHeight,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                IntPtr hbitmap = IntPtr.Zero; 
+                using (var bmpGraphics = Graphics.FromImage(screenBmp))
+                {
+                    try
+                    {
+                        bmpGraphics.CopyFromScreen(0, 0, 0, 0, screenBmp.Size);
+                        hbitmap = screenBmp.GetHbitmap();
+                        return new ImageBrush(Imaging.CreateBitmapSourceFromHBitmap(
+                            hbitmap,
+                            IntPtr.Zero,
+                            Int32Rect.Empty,
+                            BitmapSizeOptions.FromEmptyOptions()));
+                    }
+                    finally
+                    {
+                        DeleteObject(hbitmap);
+                    }
+                }
+            }
         }
     }
 }
